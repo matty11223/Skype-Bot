4599e69c20f2e6796bfd33016d49fd08f0b579f3
Matt <matty11223@gmail.com>
2014-10-24 02:15:26 +0100

    Working
    Just alittle bug fix

---
+++ Eternoz Skype bot/Eternoz Skype bot/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.FormSkin1 = New Eternoz_Skype_bot.FormSkin()
+        Me.FlatClose1 = New Eternoz_Skype_bot.FlatClose()
+        Me.FlatTabControl1 = New Eternoz_Skype_bot.FlatTabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.FlatLabel2 = New Eternoz_Skype_bot.FlatLabel()
+        Me.FlatButton3 = New Eternoz_Skype_bot.FlatButton()
+        Me.FlatLabel1 = New Eternoz_Skype_bot.FlatLabel()
+        Me.FlatTextBox1 = New Eternoz_Skype_bot.FlatTextBox()
+        Me.FlatButton2 = New Eternoz_Skype_bot.FlatButton()
+        Me.FlatButton1 = New Eternoz_Skype_bot.FlatButton()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.TabPage5 = New System.Windows.Forms.TabPage()
+        Me.FlatMini1 = New Eternoz_Skype_bot.FlatMini()
+        Me.FormSkin1.SuspendLayout()
+        Me.FlatTabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Controls.Add(Me.FlatTabControl1)
+        Me.FormSkin1.Controls.Add(Me.FlatMini1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.SandyBrown
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(694, 392)
+        Me.FormSkin1.TabIndex = 2
+        Me.FormSkin1.Text = "Pozdrav & Eternally's Skype Bot"
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(664, 13)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 3
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatTabControl1
+        '
+        Me.FlatTabControl1.ActiveColor = System.Drawing.Color.SandyBrown
+        Me.FlatTabControl1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.FlatTabControl1.Controls.Add(Me.TabPage1)
+        Me.FlatTabControl1.Controls.Add(Me.TabPage2)
+        Me.FlatTabControl1.Controls.Add(Me.TabPage3)
+        Me.FlatTabControl1.Controls.Add(Me.TabPage4)
+        Me.FlatTabControl1.Controls.Add(Me.TabPage5)
+        Me.FlatTabControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.FlatTabControl1.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.FlatTabControl1.ItemSize = New System.Drawing.Size(120, 40)
+        Me.FlatTabControl1.Location = New System.Drawing.Point(0, 47)
+        Me.FlatTabControl1.Name = "FlatTabControl1"
+        Me.FlatTabControl1.SelectedIndex = 0
+        Me.FlatTabControl1.Size = New System.Drawing.Size(694, 345)
+        Me.FlatTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
+        Me.FlatTabControl1.TabIndex = 2
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.TabPage1.Controls.Add(Me.FlatLabel2)
+        Me.TabPage1.Controls.Add(Me.FlatButton3)
+        Me.TabPage1.Controls.Add(Me.FlatLabel1)
+        Me.TabPage1.Controls.Add(Me.FlatTextBox1)
+        Me.TabPage1.Controls.Add(Me.FlatButton2)
+        Me.TabPage1.Controls.Add(Me.FlatButton1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 44)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(686, 297)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Message Spammer"
+        '
+        'FlatLabel2
+        '
+        Me.FlatLabel2.AutoSize = True
+        Me.FlatLabel2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel2.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlatLabel2.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel2.Location = New System.Drawing.Point(8, 205)
+        Me.FlatLabel2.Name = "FlatLabel2"
+        Me.FlatLabel2.Size = New System.Drawing.Size(0, 25)
+        Me.FlatLabel2.TabIndex = 6
+        '
+        'FlatButton3
+        '
+        Me.FlatButton3.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton3.BaseColor = System.Drawing.Color.SandyBrown
+        Me.FlatButton3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton3.Location = New System.Drawing.Point(452, 245)
+        Me.FlatButton3.Name = "FlatButton3"
+        Me.FlatButton3.Rounded = False
+        Me.FlatButton3.Size = New System.Drawing.Size(226, 43)
+        Me.FlatButton3.TabIndex = 4
+        Me.FlatButton3.Text = "Set Contact To Spam"
+        Me.FlatButton3.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatLabel1
+        '
+        Me.FlatLabel1.AutoSize = True
+        Me.FlatLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatLabel1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlatLabel1.ForeColor = System.Drawing.Color.White
+        Me.FlatLabel1.Location = New System.Drawing.Point(8, 14)
+        Me.FlatLabel1.Name = "FlatLabel1"
+        Me.FlatLabel1.Size = New System.Drawing.Size(173, 25)
+        Me.FlatLabel1.TabIndex = 3
+        Me.FlatLabel1.Text = "Message To Spam :"
+        '
+        'FlatTextBox1
+        '
+        Me.FlatTextBox1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatTextBox1.Location = New System.Drawing.Point(8, 42)
+        Me.FlatTextBox1.MaxLength = 200
+        Me.FlatTextBox1.Multiline = True
+        Me.FlatTextBox1.Name = "FlatTextBox1"
+        Me.FlatTextBox1.ReadOnly = True
+        Me.FlatTextBox1.Size = New System.Drawing.Size(670, 131)
+        Me.FlatTextBox1.TabIndex = 2
+        Me.FlatTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.FlatTextBox1.TextColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.FlatTextBox1.UseSystemPasswordChar = False
+        '
+        'FlatButton2
+        '
+        Me.FlatButton2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton2.BaseColor = System.Drawing.Color.SandyBrown
+        Me.FlatButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton2.Location = New System.Drawing.Point(309, 246)
+        Me.FlatButton2.Name = "FlatButton2"
+        Me.FlatButton2.Rounded = False
+        Me.FlatButton2.Size = New System.Drawing.Size(137, 43)
+        Me.FlatButton2.TabIndex = 1
+        Me.FlatButton2.Text = "Change Time"
+        Me.FlatButton2.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton1.BaseColor = System.Drawing.Color.SandyBrown
+        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton1.Location = New System.Drawing.Point(8, 246)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Rounded = False
+        Me.FlatButton1.Size = New System.Drawing.Size(295, 43)
+        Me.FlatButton1.TabIndex = 0
+        Me.FlatButton1.Text = "Start Spamming"
+        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.TabPage2.Location = New System.Drawing.Point(4, 44)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(686, 297)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Call Spam"
+        '
+        'TabPage3
+        '
+        Me.TabPage3.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.TabPage3.Location = New System.Drawing.Point(4, 44)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(686, 297)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Contact Bomber (May Not work)"
+        '
+        'TabPage4
+        '
+        Me.TabPage4.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.TabPage4.Location = New System.Drawing.Point(4, 44)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(686, 297)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Scripts"
+        '
+        'TabPage5
+        '
+        Me.TabPage5.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.TabPage5.Location = New System.Drawing.Point(4, 44)
+        Me.TabPage5.Name = "TabPage5"
+        Me.TabPage5.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage5.Size = New System.Drawing.Size(686, 297)
+        Me.TabPage5.TabIndex = 4
+        Me.TabPage5.Text = "About & Credits"
+        '
+        'FlatMini1
+        '
+        Me.FlatMini1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatMini1.BackColor = System.Drawing.Color.White
+        Me.FlatMini1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatMini1.Font = New System.Drawing.Font("Marlett", 12.0!)
+        Me.FlatMini1.Location = New System.Drawing.Point(640, 13)
+        Me.FlatMini1.Name = "FlatMini1"
+        Me.FlatMini1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatMini1.TabIndex = 1
+        Me.FlatMini1.Text = "FlatMini1"
+        Me.FlatMini1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(694, 392)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Eternoz Skype Bot"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.FlatTabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents FormSkin1 As Eternoz_Skype_bot.FormSkin
+    Friend WithEvents FlatTabControl1 As Eternoz_Skype_bot.FlatTabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents FlatMini1 As Eternoz_Skype_bot.FlatMini
+    Friend WithEvents FlatClose1 As Eternoz_Skype_bot.FlatClose
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
+    Friend WithEvents FlatButton3 As Eternoz_Skype_bot.FlatButton
+    Friend WithEvents FlatLabel1 As Eternoz_Skype_bot.FlatLabel
+    Friend WithEvents FlatTextBox1 As Eternoz_Skype_bot.FlatTextBox
+    Friend WithEvents FlatButton2 As Eternoz_Skype_bot.FlatButton
+    Friend WithEvents FlatButton1 As Eternoz_Skype_bot.FlatButton
+    Friend WithEvents TabPage5 As System.Windows.Forms.TabPage
+    Friend WithEvents FlatLabel2 As Eternoz_Skype_bot.FlatLabel
+
+End Class
---
+++ Eternoz Skype bot/Eternoz Skype bot/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports SKYPE4COMLib
+Public Class Form1
+    Public WithEvents skype As New SKYPE4COMLib.Skype
+    Private Sub FlatButton1_Click(sender As Object, e As EventArgs)
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+    End Sub
+
+    Private Sub FormSkin1_Click(sender As Object, e As EventArgs) Handles FormSkin1.Click
+
+    End Sub
+
+    Private Sub FlatButton2_Click(sender As Object, e As EventArgs) Handles FlatButton2.Click
+        
+
+    End Sub
+End Class
